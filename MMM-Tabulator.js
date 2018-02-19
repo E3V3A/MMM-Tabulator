dfe3adb48d0741e3080e26979e33d98dde216ed0
E3V3A <xdae3v3a@gmail.com>
2018-02-19 22:11:30 +0200

    changed/updated core :broken_heart:

--- MMM-Tabulator.js
+++ MMM-Tabulator.js
@@ -1,144 +1,181 @@
 /* -----------------------------------------------------------
  * Module:       MMM-Tabulator
+ * FileName:     MMM-Tabulator.js
  * Author:       E:V:A
  * License:      MIT
- * Date:         2018-02-12
- * Version:      1.0
+ * Date:         2018-02-19
+ * Version:      0.0.2
  * Description:  A MagicMirror Demo module to use Tabulator
- * URL:          https://
+ * URL:          https://github.com/E3V3A/MMM-Tabulator
+ * Template:     https://github.com/roramirez/MagicMirror-Module-Template/
  * -----------------------------------------------------------
  */
 
-Module.register("MMM-Tabulator", {
-	defaults: {
-		updateInterval: 60000,
-		retryDelay: 5000
-	},
-
-	requiresVersion: "2.1.0", // Required version of MagicMirror
-
-	start: function() {
-		var self = this;
-		var dataRequest = null;
-		var dataNotification = null;
-
-		// Flag to check if module is loaded
-		this.loaded = false;
-
-		// Schedule the update timer
-		this.getData();
-		setInterval(function() {
-			self.updateDom();
-		}, this.config.updateInterval);
-	},
-
-	getData: function() {
-		var self = this;
-
-		var urlApi = "https://jsonplaceholder.typicode.com/posts/1";
-		var retry = true;
-
-		var dataRequest = new XMLHttpRequest();
-		dataRequest.open("GET", urlApi, true);
-		dataRequest.onreadystatechange = function() {
-			console.log(this.readyState);
-			if (this.readyState === 4) {
-				console.log(this.status);
-				if (this.status === 200) {
-					self.processData(JSON.parse(this.response));
-				} else if (this.status === 401) {
-					self.updateDom(self.config.animationSpeed);
-					Log.error(self.name, this.status);
-					retry = false;
-				} else {
-					Log.error(self.name, "Could not load data.");
-				}
-				if (retry) {
-					self.scheduleUpdate((self.loaded) ? -1 : self.config.retryDelay);
-				}
-			}
-		};
-		dataRequest.send();
-	},
-
-	scheduleUpdate: function(delay) {
-		var nextLoad = this.config.updateInterval;
-		if (typeof delay !== "undefined" && delay >= 0) {
-			nextLoad = delay;
-		}
-		nextLoad = nextLoad ;
-		var self = this;
-		setTimeout(function() {
-			self.getData();
-		}, nextLoad);
-	},
-
-	getDom: function() {
-		var self = this;
-
-		var wrapper = document.createElement("div");
-		wrapper.idName = 'flighttable'; //E3V3A
-		if (this.dataRequest) {
-			var wrapperDataRequest = document.createElement("div");
-			// check format https://jsonplaceholder.typicode.com/posts/1
-			wrapperDataRequest.innerHTML = this.dataRequest.title;
-			var labelDataRequest = document.createElement("label");
-			labelDataRequest.innerHTML = "Title";
-
-			wrapper.appendChild(labelDataRequest);
-			wrapper.appendChild(wrapperDataRequest);
-		}
-
-		// Data from helper
-		if (this.dataNotification) {
-			var wrapperDataNotification = document.createElement("div");
-			// translations  + datanotification
-			wrapperDataNotification.innerHTML =  "Update" + ": " + this.dataNotification.date;
-			wrapper.appendChild(wrapperDataNotification);
-		}
-		return wrapper;
-	},
+//app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 /*
+//-----------------------------------------------------
+// Tabulator Requires:
 "node_modules/jquery/dist/jquery.min.js",
 "node_modules/jquery-ui-dist/jquery-ui.min.js",
 "node_modules/jquery-ui-dist/jquery-ui.css",
 "node_modules/jquery.tabulator/dist/js/tabulator.js",
 "node_modules/jquery.tabulator/dist/css/tabulator.css",
+//-----------------------------------------------------
 */
 
-	//app.use('/node_modules', express.static(__dirname + '/node_modules'));
+Module.register('MMM-Tabulator',{
+
+	defaults: {
+		//header: "Some Flights",
+		updateInterval: 10000 //30*60*1000 // [ms] Read the file every 30 min
+		//fileUrl: "file:///home/pi/MagicMirror/modules/MMM-backlog/demo.json"
+	},
+
+	requiresVersion: "2.1.0",
 
 	getScripts: function() {
 		return [
-/*			"modules/MMM-tabulator/node_modules/jquery/dist/jquery.min.js",
-			"node_modules/jquery-ui-dist/jquery-ui.min.js",
-			"node_modules/jquery.tabulator/dist/js/tabulator.js",
-*/		];
+			this.file('node_modules/jquery/dist/jquery.min.js'),
+			this.file('node_modules/jquery-ui-dist/jquery-ui.min.js'),
+			this.file('node_modules/jquery.tabulator/dist/js/tabulator.js')
+//			'modules/MMM-Tabulator/node_modules/jquery/dist/jquery.min.js',
+//			'modules/MMM-Tabulator/node_modules/jquery-ui-dist/jquery-ui.min.js',
+//			'modules/MMM-Tabulator/node_modules/jquery.tabulator/dist/js/tabulator.js'
+		]
 	},
 
-	getStyles: function () {
+	getStyles: function() {
 		return [
-/*			"node_modules/jquery-ui-dist/jquery-ui.css",
-			"node_modules/jquery.tabulator/dist/css/tabulator.css",
-*/			"MMM-Tabulator.css",
+			this.file('node_modules/jquery-ui-dist/jquery-ui.css'),
+			this.file('node_modules/jquery.tabulator/dist/css/tabulator.css'),
+//			"node_modules/jquery-ui-dist/jquery-ui.css",
+//			"node_modules/jquery.tabulator/dist/css/tabulator.css",
+			"MMM-Tabulator.css"
 		];
 	},
 
-	processData: function(data) {
-		var self = this;
-		this.dataRequest = data;
-		if (this.loaded === false) { self.updateDom(self.config.animationSpeed) ; }
-		this.loaded = true;
-		this.sendSocketNotification("MMM-Tabulator-NOTIFICATION_TEST", data);
+	start: function() {
+		console.log("MMM-Tabulator.js: Sending START notification...");
+		this.loaded = false;
+		this.sendSocketNotification('START', this.config);
 	},
 
-	// socketNotificationReceived from helper
-	socketNotificationReceived: function (notification, payload) {
-		if(notification === "MMM-Tabulator-NOTIFICATION_TEST") {
-			// set dataNotification
-			this.dataNotification = payload;
+
+	notificationReceived: function(notification, payload, sender) {
+		var self = this;
+		//if ( notification == "DOM_OBJECTS_CREATED" ) {
+		if ( notification == "ALL_MODULES_STARTED" ) {
+			//var loadres = self.loadTabulate(this.data);
+			var loadres = self.loadTabulate();
+			//this.loadTabulate();
+			console.log("loadTabulate Result: " + loadres );
+			console.log("All DOM\'s created!");
+			return true;
+		}
+		return false;
+	},
+
+	getTranslations: function() { return false; }, // Nothing to translate
+
+	getDom: function() {
+		var w = document.createElement("div"); // Let w be a "wrapper"
+		w.id = "flighttable";
+
+		if (!this.loaded) {
+			w.innerHTML = "Loading...";
+			w.className = "dimmed light small";
+			return w;
+		}
+		if (!this.data) {
+			w.innerHTML = "No data!";
+			w.className = "dimmed light small";
+			return w;
+		}
+		w.innerHTML = "Waiting for Tabulator...";
+//		w.innerHTML = this.data;
+		return w;
+	},
+
+ 	socketNotificationReceived: function(notification, payload) {
+    		if (notification === "STARTED") {
 			this.updateDom();
 		}
+		else if (notification === "DATA") {
+			console.log("MMM-Tabulator: DATA received!");
+			this.loaded = true;
+			this.data = payload;
+			//this.processData(JSON.parse(payload));
+			this.updateDom();
+    		}
 	},
+
+	loadTabulate: function() {
+	//=======================================================================================
+	'use strict'
+
+	//var realData = this.data;
+
+	Tabulator.extendExtension("format", "formatters", {
+	    ft2mt:function(cell, formatterParams){              // Feet to Meters
+	        return  (0.3048*cell.getValue()).toFixed(0);
+	    },
+	    kn2km:function(cell, formatterParams){              // Knots to Kilometers
+	        return  (1.852*cell.getValue()).toFixed(0);
+	    },
+	    ep2time:function(cell, formatterParams){            // POSIX epoch to hh:mm:ss
+	        let date = new Date(cell.getValue());
+	        return date.toLocaleString('en-GB', { hour:'numeric', minute:'numeric', second:'numeric', hour12:false } );
+	    },
+	});
+
+	$("#flighttable").tabulator({
+	    height:205,                         // Set height of table, this enables the Virtual DOM and improves render speed
+	    //layout:"fitColumns",                // Fit columns to width of table (optional)
+	    //headerSort:false,                   // Disable header sorter
+	    resizableColumns:false,             // Disable column resize
+	    responsiveLayout:true,              // Enable responsive layouts
+	    placeholder:"No Data Available",    // Display message to user on empty table
+	    initialSort:[                       // Define the sort order:
+	        {column:"altitude",     dir:"asc"},     // 1'st
+	        //{column:"flight",     dir:"desc"},    // 2'nd
+	        //{column:"bearing",    dir:"asc"},     // 3'rd
+	    ],
+	    columns:[
+	        {title:"Flight",        field:"flight",         headerSort:false, sortable:false, responsive:0, align:"left"}, // , width:250},
+	        {title:"CallSig",       field:"callsign",       headerSort:false, sortable:false, responsive:3},
+	        {title:"From",          field:"origin",         headerSort:false, sortable:false, responsive:0},
+	        {title:"To",            field:"destination",    headerSort:false, sortable:false, responsive:0},
+	        {title:"Speed",         field:"speed",          headerSort:false, sortable:false, responsive:2, formatter:"kn2km"}, // [km/h]
+	        {title:"Bearing",       field:"bearing",        headerSort:false, sortable:false, responsive:1},
+	        {title:"Alt [m]",       field:"altitude",       headerSort:false, sortable:false, responsive:0, formatter:"ft2mt", align:"right", sorter:"number"},
+	        //{title:"Alt [m]",       field:"altitude",       sortable:true,  responsive:0, align:"right", sorter:"number", mutateType:"data", mutator:ft2met"},
+	        // Additional items:
+	        {title:"F24id",         field:"id",             headerSort:false, sortable:false, visible:false},
+	        {title:"RegID",         field:"registration",   headerSort:false, sortable:false, visible:false},
+	        {title:"Model",         field:"model",          headerSort:false, sortable:false, visible:true, responsive:1},
+	        {title:"ModeS",         field:"modeSCode",      headerSort:false, sortable:false, visible:false},
+	        {title:"Radar",         field:"radar",          headerSort:false, sortable:false, visible:false},
+	        {title:"Lat",           field:"latitude",       headerSort:false, sortable:false, visible:false},
+	        {title:"Lon",           field:"longitude",      headerSort:false, sortable:false, visible:false},
+
+	        {title:"Time",          field:"timestamp",      headerSort:false, sortable:false, visible:true, responsive:1, formatter:"ep2time"},
+	        {title:"RoC [ft/m]",    field:"climb",          headerSort:false, sortable:false, visible:false},
+	        {title:"Squawk",        field:"squawk",         headerSort:false, sortable:false, visible:true, responsive:1}, // formatter:"sqCheck"},
+	        {title:"isGND",         field:"ground",         headerSort:false, sortable:false, visible:false},
+	        {title:"isGlider",      field:"glider",         headerSort:false, sortable:false, visible:false},
+	    ],
+	});
+
+	var realData = this.data;
+
+	$("#flighttable").tabulator("setData", realData);
+
+	$(window).resize(function(){
+		$("#flighttable").tabulator("redraw");
+	});
+	//=======================================================================================
+		return "WIP (Did it work?)";
+	}
+
 });
--- node_helper.js
+++ node_helper.js
@@ -3,37 +3,99 @@
  * File:         nodehelper.js
  * Author:       E:V:A
  * License:      MIT
- * Date:         2018-02-12
- * Version:      1.0
+ * Date:         2018-02-19
+ * Version:      0.0.2
  * Description:  A MagicMirror Demo module for using Tabulator
  * URL:          https://github.com/E3V3A/MMM-Tabulator
  * -----------------------------------------------------------
+ * also see:
+ *   https://stackoverflow.com/questions/32621988/electron-jquery-is-not-defined?rq=1
+ *   https://electronjs.org/docs/faq
+ *   https://github.com/MichMich/MagicMirror/issues/440
+ *   https://github.com/fewieden/MMM-voice/blob/master/node_helper.js
  */
 
-var NodeHelper = require("node_helper");
-var moment = require("moment");
+/*
+"node_modules/jquery/dist/jquery.min.js",
+"node_modules/jquery-ui-dist/jquery-ui.min.js",
+"node_modules/jquery-ui-dist/jquery-ui.css",
+"node_modules/jquery.tabulator/dist/js/tabulator.js",
+"node_modules/jquery.tabulator/dist/css/tabulator.css",
+*/
+
+const NodeHelper = require("node_helper");
+const moment = require("moment");
+const fs = require('fs');
+//const path = require('path');
+
+const myfile = 'modules/MMM-Tabulator/demo.json'; //'modules/MMM-Tabulator/demo.json'
+
+/*
+const $ = require('jquery');
+//var $ = require('jquery/dist/jquery.min');
+global.jQuery = $;
+const jqueryui = require('jquery-ui-dist/jquery-ui.min');
+const jquerytabulator = require('jquery.tabulator');
+const tabulator = require('jquery.tabulator/dist/js/tabulator');
+*/
+
+/*
+var $ = require('jquery.min');
+//global.jQuery = $;
+window.jQuery = $;
+require('jquery-ui-dist/jquery-ui.min.js');
+require('jquery-ui-dist/jquery-ui.css');
+require('jquery.tabulator/dist/js/tabulator.min.js');
+require('jquery.tabulator/dist/css/tabulator.min.css');
+*/
 
 module.exports = NodeHelper.create({
 
 	start: function() {
-		console.log("Starting module: " + this.name);
+                this.started = false;
+                this.config = null;
+                console.log("Started MMM-Tabulator:node_helper.js");
+		//console.log("\nStarting module: " + this.name);
+		//console.log(`\nStarting module helper: ${this.name}`);
 	},
 
-	socketNotificationReceived: function(notification, config) {
-		if (notification === "MMM-Tabulator-NOTIFICATION_TEST") {
-			console.log("Working notification system. Notification:", notification, "payload: ", payload);
+        readData: function(){
+                fs.readFile( myfile, 'utf8', (err, data) => {
+                        if (err) throw err;
+                        //console.log("\nRead file data: " + data);
+                        console.log("Read file data!");
+                        this.sendSocketNotification("DATA", data);
+                });
+        },
+
+	//socketNotificationReceived: function(notification, config) {
+        socketNotificationReceived: function(notification, payload) {
+                var self = this;
+                if (notification === "START") {
+                        this.config = payload;
+                        this.readData();
+                        setInterval(() => { this.readData(); }, this.config.updateInterval);
+                }
+
+/*			console.log("MMMT:node_helper.js Received notification: " + notification ); //, "\npayload: ", payload);
 			this.sendNotificationTest(this.anotherFunction());
 		}
-	},
-
+*/
+	}
+/*,
 	sendNotificationTest: function(payload) {
-		this.sendSocketNotification("MMM-Tabulator-NOTIFICATION_TEST", payload);
+		console.log("MMMT:node_helper.js Sending MMMT_PING with payload: " + payload );
+		this.sendSocketNotification("MMMT_PING", payload);
 	},
 
+	anotherFunction: function() {
+		return {date: new Date()};
+	}
+*/
 });
 
-function degreesToDirection(num) {
+/*function degreesToDirection(num) {
 	var val = Math.floor((num / 22.5) + 0.5);
 	var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
 	return arr[(val % 16)];
-}
+}*/
