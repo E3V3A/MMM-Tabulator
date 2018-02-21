a895653d28fedd64227666cdfdd6e5cba7117d41
E3V3A <xdae3v3a@gmail.com>
2018-02-21 15:17:17 +0200

    :heart:

--- node_helper.js
+++ node_helper.js
@@ -1,87 +1,65 @@
 /* -----------------------------------------------------------
  * Module:       MMM-Tabulator
- * File:         nodehelper.js
+ * FileName:     node_helper.js
  * Author:       E:V:A
  * License:      MIT
- * Date:         2018-02-19
- * Version:      0.0.2
+ * Date:         2018-02-21
+ * Version:      1.0.0
  * Description:  A MagicMirror Demo module for using Tabulator
- * URL:          https://github.com/E3V3A/MMM-Tabulator
+ * Format:       4-space TAB's (no TAB chars), mixed quotes
+ *
+ * URL1:         https://github.com/E3V3A/MMM-Tabulator
+ * URL2:         https://github.com/olifolkerd/tabulator
  * -----------------------------------------------------------
- * also see:
+ * Tabulator Requires:
+ *      "node_modules/jquery/dist/jquery.min.js",
+ *      "node_modules/jquery-ui-dist/jquery-ui.min.js",
+ *      "node_modules/jquery-ui-dist/jquery-ui.css",
+ *      "node_modules/jquery.tabulator/dist/js/tabulator.js",
+ *      "node_modules/jquery.tabulator/dist/css/tabulator.css",
+ * -----------------------------------------------------------
+ * See:
  *   https://stackoverflow.com/questions/32621988/electron-jquery-is-not-defined?rq=1
  *   https://electronjs.org/docs/faq
- *   https://github.com/MichMich/MagicMirror/issues/440
- *   https://github.com/fewieden/MMM-voice/blob/master/node_helper.js
- */
-
-/*
-"node_modules/jquery/dist/jquery.min.js",
-"node_modules/jquery-ui-dist/jquery-ui.min.js",
-"node_modules/jquery-ui-dist/jquery-ui.css",
-"node_modules/jquery.tabulator/dist/js/tabulator.js",
-"node_modules/jquery.tabulator/dist/css/tabulator.css",
 */
+
+'use strict';
 
 const NodeHelper = require("node_helper");
-const moment = require("moment");
 const fs = require('fs');
-//const path = require('path');
-
-const myfile = 'modules/MMM-Tabulator/demo.json'; //'modules/MMM-Tabulator/demo.json'
-
-/*
-const $ = require('jquery');
-//var $ = require('jquery/dist/jquery.min');
-global.jQuery = $;
-const jqueryui = require('jquery-ui-dist/jquery-ui.min');
-const jquerytabulator = require('jquery.tabulator');
-const tabulator = require('jquery.tabulator/dist/js/tabulator');
-*/
-
-/*
-var $ = require('jquery.min');
-//global.jQuery = $;
-window.jQuery = $;
-require('jquery-ui-dist/jquery-ui.min.js');
-require('jquery-ui-dist/jquery-ui.css');
-require('jquery.tabulator/dist/js/tabulator.min.js');
-require('jquery.tabulator/dist/css/tabulator.min.css');
-*/
 
 module.exports = NodeHelper.create({
 
-	start: function() {
-                this.started = false;
-                this.config = null;
-                console.log("Started MMM-Tabulator:node_helper.js");
-		//console.log("\nStarting module: " + this.name);
-		//console.log(`\nStarting module helper: ${this.name}`);
-	},
+    start: function() {
+        this.config = null;
+    },
 
-        readData: function(){
-                fs.readFile( myfile, 'utf8', (err, data) => {
-                        if (err) throw err;
-                        //console.log("\nRead file data: " + data);
-                        console.log("Read file data!");
-                        this.sendSocketNotification("DATA", data);
-                });
-        },
+    readData: function() {
+        const myfile = 'modules/MMM-Tabulator/demo.json'; // The API gives improper JSON
 
-	//socketNotificationReceived: function(notification, config) {
-        socketNotificationReceived: function(notification, payload) {
-                var self = this;
-                if (notification === "START") {
-                        this.config = payload;
-                        this.readData();
-                        setInterval(() => { this.readData(); }, this.config.updateInterval);
-                }
+        fs.readFile( myfile, 'utf8', (err, data) => {
+            if (err) throw err;
+            //let cleanData = JSON.stringify(data); // Fix dirty JSON
+            //console.log("\nRead file data: " + data);
+            this.sendSocketNotification("NEW_DATA", data);
+        });
+    },
 
-	}
+    socketNotificationReceived: function (notification, payload) {
+        if (notification === "REQUEST_DATA") {
+            this.config = payload;
+            this.readData();
+            setInterval(() => {
+                this.readData();
+            }, this.config.updateInterval);
+        }
+    }
+
 });
+
 // This is too beautiful not to use!
 function degToDir(num) {
-	var val = Math.floor((num / 22.5) + 0.5);
-	var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
-	return arr[(val % 16)];
+    var val = Math.floor((num / 22.5) + 0.5);
+    var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+    return arr[(val % 16)];
 }
